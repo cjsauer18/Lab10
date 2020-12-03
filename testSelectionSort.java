8e2c6f67a3764f50c677d718fa8f872569e01f06
= <cjsauer18@gmail.com>
2020-12-03 15:06:02 -0500

    postive and negative unit tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -16,27 +16,49 @@ public class testSelectionSort {
     }
 
     public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
+          int[] arr = new int[5];
+          arr[0] = 8;
+          arr[1] = 9;
+          arr[2] = 7;
+          arr[3] = 10;
+          arr[4] = 2;
 
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+          int[] Sortedarr = new int[5];
+          Sortedarr[0] = 2;
+          Sortedarr[1] = 7;
+          Sortedarr[2] = 8;
+          Sortedarr[3] = 9;
+          Sortedarr[4] = 10;
+          SelectionSort temp = new SelectionSort();
+          temp.basicSelectionSort(arr);
 
-        /** add tests to check for this unit test **/
-    }
+          /** add tests to check for this unit test **/
+          Assert.assertArrayEquals(arr, Sortedarr);
+          System.out.print("Test Positive: Passed!");
 
-    public void testNegative(){
+      }
 
-        /** Test data contains negative values only **/
-    }
+      public void testNegative(){
+          int[] arr = new int[5];
+          arr[0] = -8;
+          arr[1] = -9;
+          arr[2] = -7;
+          arr[3] = -10;
+          arr[4] = -2;
+
+          int[] Sortedarr = new int[5];
+          Sortedarr[0] = -10;
+          Sortedarr[1] = -9;
+          Sortedarr[2] = -8;
+          Sortedarr[3] = -7;
+          Sortedarr[4] = -2;
+          SelectionSort temp = new SelectionSort();
+          temp.basicSelectionSort(arr);
+          Assert.assertArrayEquals(arr, Sortedarr);
+          /** Test data contains negative values only **/
+          System.out.print("Test Negative: Passed!");
+
+      }
 
     public void testMixed(){
 
